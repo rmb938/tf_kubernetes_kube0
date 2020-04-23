fb155334da8daa2fdefc68941fb535c8c2106be0
Ryan Belgrave <ryan@belgrave.me>
2020-04-22 19:54:30 -0500

    add prometheus crds (#4)
    * add prometheus crds
    
    * make the quota nice round numbers
    
    * forgot to set name and max history
    
    * I'm dumb

--- namespaces/prometheus.tf
+++ namespaces/prometheus.tf
@@ -2,3 +2,23 @@ module "prometheus-namespace" {
   source = "../modules/namespace"
   name   = "prometheus"
 }
+
+data "helm_repository" "personal" {
+  name = "personal"
+  url  = "http://charts.rmb938.com"
+}
+
+resource "helm_release" "prometheus-operator-crde" {
+  name      = "prometheus-operator-crd"
+  namespace = module.prometheus-namespace.namespace_name
+
+  repository = data.helm_repository.personal.metadata[0].name
+  chart      = "prometheus-operator-crd"
+  version    = "0.1.0"
+
+  max_history = 10
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
