fc60cb39c8ebe9b1a2d5d453107a445e2511c0c0
Ryan Belgrave <ryan@belgrave.me>
2020-04-23 17:37:43 -0500

    update prom op crd and deploy prom op

--- namespaces/prometheus.tf
+++ namespaces/prometheus.tf
@@ -8,17 +8,32 @@ data "helm_repository" "personal" {
   url  = "http://charts.rmb938.com"
 }
 
-resource "helm_release" "prometheus-operator-crde" {
+resource "helm_release" "prometheus-operator-crd" {
   name      = "prometheus-operator-crd"
   namespace = module.prometheus-namespace.namespace_name
 
   repository = data.helm_repository.personal.metadata[0].name
   chart      = "prometheus-operator-crd"
-  version    = "0.1.0"
+  version    = "0.1.1"
 
   max_history = 10
 
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "helm_release" "prometheus-operator" {
+  name      = "prometheus-operator"
+  namespace = module.prometheus-namespace.namespace_name
+
+  repository = data.helm_repository.personal.metadata[0].name
+  chart      = "prometheus-operator"
+  version    = "0.1.1"
+
+  max_history = 10
+
+  depends_on = [
+    helm_release.prometheus-operator-crd
+  ]
 }
