2a3973d3e4a37bb43ad0422f9440416158a33c11
Ryan Belgrave <ryan@belgrave.me>
2020-04-22 19:37:53 -0500

    add prometheus crds

--- namespaces/prometheus.tf
+++ namespaces/prometheus.tf
@@ -2,3 +2,19 @@ module "prometheus-namespace" {
   source = "../modules/namespace"
   name   = "prometheus"
 }
+
+data "helm_repository" "personal" {
+  name = "personal"
+  url  = "http://charts.rmb938.com"
+}
+
+resource "helm_release" "prometheus-operator-crde" {
+  name       = "prometheus-operator-crd"
+  repository = data.helm_repository.personal.metadata[0].name
+  chart      = "prometheus-operator-crd"
+  version    = "0.1.0"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
