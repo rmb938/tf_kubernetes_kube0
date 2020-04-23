455a00ebd80ecc8e6becd56fbfe03af1d8e23630
Ryan Belgrave <ryan@belgrave.me>
2020-04-22 19:41:53 -0500

    make the quota nice round numbers

--- modules/namespace/variables.tf
+++ modules/namespace/variables.tf
@@ -5,9 +5,9 @@ variable "name" {
 variable "quota" {
   type = map
   default = {
-    "limits.cpu"      = 4        # number of cores in a pi4 b
-    "limits.memory"   = "3.7Gi"  # amount of memory in a pi4 b
-    "requests.cpu"    = 2        # half of a pi4 b
-    "requests.memory" = "1.85Gi" # half of a pi4 b
+    "limits.cpu"      = 4     # number of cores in a pi4 b
+    "limits.memory"   = "3Gi" # amount of memory in a pi4 b
+    "requests.cpu"    = 2     # half of a pi4 b
+    "requests.memory" = "1Gi" # half of a pi4 b
   }
 }
