89503d6cf045617c5560e0cd602801e7524f72bd
Ryan Belgrave <ryan@belgrave.me>
2020-10-15 15:26:13 -0500

    forgot namespace

--- resources/nfs.tf
+++ resources/nfs.tf
@@ -4,5 +4,6 @@ module "nfs-namespace" {
 }
 
 module "nfs-client-provisioner" {
-  source = "./nfs-client-provisioner"
+  source    = "./nfs-client-provisioner"
+  namespace = module.nfs-namespace.namespace_name
 }
